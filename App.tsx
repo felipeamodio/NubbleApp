c6b290370601579037e7264b7e6735233f718c2d
Felipe <felipeaamodio@gmail.com>
2023-10-26 18:56:21 -0300

    improving text input component

--- App.tsx
+++ App.tsx
@@ -3,9 +3,9 @@ import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from './src/theme/theme';
-import { Box } from './src/components/Box/Box';
 import { Button } from './src/components/Button/Button';
 import { TextInput } from './src/components/TextInput/TextInput';
+import { Icon } from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
@@ -15,13 +15,9 @@ function App(): JSX.Element {
           <Text preset="headingLarge" italic mb="s8">Olá</Text>
           <Text preset="paragraphLarge" italic mb="s40">Digite seu e-mail e senha para entrar</Text>
 
-          <Box mb="s20">
-            <TextInput label='Email' placeholder='Digite seu e-mail' />
-          </Box>
+            <TextInput errorMessage='mensagem de error' label='Email' placeholder='Digite seu e-mail' boxProps={{mb: "s20"}} />
 
-          <Box>
-            <TextInput label='Senha' placeholder='Digite sua senha' />
-          </Box>
+            <TextInput label='Senha' placeholder='Digite sua senha' RightComponent={<Icon name='eyeOn' color='gray2' />} boxProps={{mb: "s10"}} />
 
           <Text color="primary" preset="paragraphSmall" bold mt="s10">Esqueci minha senha</Text>
 
--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -1,39 +1,66 @@
 /* eslint-disable react/react-in-jsx-scope */
-import {TextInput as RNTextInput, TextInputProps as RNTextInputProps, TextStyle} from 'react-native';
+import { useRef } from 'react';
+import {Pressable, TextInput as RNTextInput, TextInputProps as RNTextInputProps, TextStyle} from 'react-native';
 import { useAppTheme } from '../../hooks/useAppTheme';
 import { Box, BoxProps } from '../Box/Box';
 import { $fontsFamily, $fontSizes, Text } from '../Text/Text';
 
 interface TextInputProps extends RNTextInputProps {
     label: string;
+    errorMessage?: string;
+    RightComponent?: React.ReactElement;
+    boxProps?: BoxProps;
 }
 
-const $textInputContainer: BoxProps = {
-    borderWidth: 1,
-    padding: 's16',
-    borderColor: 'gray4',
-    borderRadius: 's12',
-}
-
+// flexGrow faz com que ele ocupe todo o espaço dentro do box e o flexShrink faz com que ele não sobrescreva por cima
 const $textInputStyle: TextStyle = {
-    borderWidth: 1,
+    flexGrow: 1,
+    flexShrink: 1,
     padding: 0,
     fontFamily: $fontsFamily.regular,
     ...$fontSizes.paragraphMedium,
 }
 
-export function TextInput({label, ...rnTextInputProps}: TextInputProps){
+export function TextInput({label, errorMessage, RightComponent, boxProps, ...rnTextInputProps}: TextInputProps){
     const {colors} = useAppTheme();
+    const inputRef = useRef<RNTextInput>(null);
+
+    function focusInput(){
+        //current é a referencia atual e ela está undefined por isso tem o ?
+        inputRef.current?.focus();
+    }
+
+    //passar esse Pressable pra ele pegar a referencia da caixa toda, não só da parte onde está o input
+
+    const $textInputContainer: BoxProps = {
+        flexDirection: 'row',
+        borderWidth: errorMessage ? 2 : 1,
+        padding: 's16',
+        borderColor: errorMessage ? 'error' : 'gray4',
+        borderRadius: 's12',
+    }
+
     return (
-        <Box>
-            <Text preset='paragraphMedium' marginBottom='s4'>{label}</Text>
-            <Box {...$textInputContainer}>
-                <RNTextInput
-                    style={$textInputStyle}
-                    placeholderTextColor={colors.gray2}
-                    {...rnTextInputProps}
-                />
-            </Box>
+        <Box {...boxProps}>
+            <Pressable onPress={focusInput}>
+                <Text preset='paragraphMedium' marginBottom='s4'>{label}</Text>
+                <Box {...$textInputContainer}>
+                    <RNTextInput
+                        style={$textInputStyle}
+                        placeholderTextColor={colors.gray2}
+                        ref={inputRef}
+                        {...rnTextInputProps}
+                    />
+                    {RightComponent &&(
+                    <Box ml='s16' justifyContent='center'>
+                        {RightComponent}
+                    </Box>
+                    )}
+                </Box>
+                {errorMessage && (
+                    <Text preset='paragraphSmall' bold color='error'>{errorMessage}</Text>
+                )}
+            </Pressable>
         </Box>
     )
 }
