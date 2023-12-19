5f5f1946bc46f808c36f11237b5e1fbfe4f576f9
Felipe <felipeaamodio@gmail.com>
2023-12-19 20:47:31 -0300

    mocking post list

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,11 +1,19 @@
+import { useEffect, useState } from 'react';
+
+import { Post, postService } from '@domain';
+
 import { Button, Screen, Text } from '@components';
 import { AppTabScreenProps } from '@routes';
 
+
 export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>){
+    const [postList, setPostList] = useState<Post[]>([]);
+
+    useEffect(() => {
+        postService.getList().then(list => setPostList(list));
+    }, []);
+
     return (
-        <Screen>
-            <Text preset="headingLarge">Home Screen</Text>
-            <Button title="Settings" onPress={() => navigation.navigate('SettingsScreen')} />
-        </Screen>
+        <Screen />
     );
 }
