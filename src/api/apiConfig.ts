5b41b9165db324457acfbba294adb7af0c0f46d7
Felipe <felipeaamodio@gmail.com>
2024-03-08 16:28:28 -0300

    created domain, API and service for post comment

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -4,6 +4,6 @@ export const api = axios.create({
     baseURL: 'http://127.0.0.1:3333/',
     headers: {
         Authorization:
-            'Bearer MQ.Y9uzj1sbZ3_-FfWP8-gkYnIl5sa263_mEik7kJEVJNv7nh6raKZj9VFvaHgv',
+            'Bearer MQ.8B9sFvzyvlZZoWi1HTu3ap2WrZ-uqyaHZpsIFWWVIAE7tGjE2pe110xIluvv',
     },
 });
