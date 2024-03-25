cae7fbdc7fe328a3a6d71291632be95d11433c7a
Felipe <felipeaamodio@gmail.com>
2024-03-25 18:42:32 -0300

    fix imports on post comment files

--- src/screens/app/PostCommentScreen/PostCommentScreen.tsx
+++ src/screens/app/PostCommentScreen/PostCommentScreen.tsx
@@ -1,7 +1,9 @@
-import { usePostCommentList } from 'src/domain/PostComment/useCases/usePostCommentList';
+
+import { usePostCommentList } from '@domain';
 
 import { Box, Screen, Text } from '@components';
 import { AppScreenProps } from '@routes';
+
 
 export function PostCommentScreen({route}: AppScreenProps<'PostCommentScreen'>){
     const postId = route.params.postId;
